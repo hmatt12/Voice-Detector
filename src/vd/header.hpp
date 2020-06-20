eb37a0625dd8cbb80efef3224cb183d54e06cf09
Ian Boll <iangboll@vt.edu>
2020-06-20 11:14:03 -0400

    basic project structure & build file

---
+++ src/vd/header.hpp
@@ -0,0 +1,10 @@
+#ifndef VD_HEADER_H_
+#define VD_HEADER_H_
+
+template<typename T>
+class HeaderObject {
+private:
+    T field;
+};
+
+#endif // VD_HEADER_H_
---
+++ src/vd/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vd/header.hpp>
+
+int main(int argc, char **argv) {
+    HeaderObject<int> thing;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
